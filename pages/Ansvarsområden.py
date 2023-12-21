2239411b788bf0ff958bb8548b77b66911a67bad
Tobias Löfgren <tobias.lofgren@svt.se>
2023-12-21 17:09:30 +0100

    add static image serving

--- pages/Ansvarsområden.py
+++ pages/Ansvarsområden.py
@@ -11,7 +11,23 @@ from utils import on_data_edited, setup_gsheets_connection, setup_initial_sessio
 def print_responsibilities():
     # Load the initial pack list
     st.session_state.responsibilities, conn = setup_gsheets_connection("responsibilities")
+    st.image("images/arvid.jpeg", width=300)
+    st.image("static/arvid.jpeg", width=300)
+    
+    st.session_state.responsibilities["Person"] = pd.DataFrame(
+        {
+            "Person": [
+                "app/static/arvid.jpeg",
+                "app/static/ellen.jpeg",
+                "app/static/tobias.jpeg",
+                "app/static/pernilla.jpeg",
+                "app/static/felix.jpeg",
+                "app/static/alla.png",
+            ],
+        }
+    )
 
+    column_config={"Person": st.column_config.ImageColumn() }
 
     # Display the current pack list in a data editor
     st.data_editor(st.session_state.responsibilities.reset_index(drop=True), 
@@ -19,7 +35,8 @@ def print_responsibilities():
                                                 on_change=on_data_edited,
                                                 args=("responsibilities", conn),
                                                 key="responsibilities_changes",
-                                                num_rows="dynamic",)
+                                                column_config=column_config,
+                                                hide_index=True)
 
 
 setup_initial_session_state()
